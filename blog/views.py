1b1616b932178656696ab300238bb1ce0170f8d6
Flynotfly <Alteria2004@gmail.com>
2024-07-22 08:58:02 +0400

    disable post_list view

--- blog/views.py
+++ blog/views.py
@@ -11,24 +11,24 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    # Pagination with 5 post per page
-    paginator = Paginator(post_list, 5)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        # get last page if page_number out of range
-        posts = paginator.page(paginator.num_pages)
-
-    return render(
-        request,
-        'blog/post/list.html',
-        {'posts': posts}
-    )
+# def post_list(request):
+#     post_list = Post.published.all()
+#     # Pagination with 5 post per page
+#     paginator = Paginator(post_list, 5)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # get last page if page_number out of range
+#         posts = paginator.page(paginator.num_pages)
+#
+#     return render(
+#         request,
+#         'blog/post/list.html',
+#         {'posts': posts}
+#     )
 
 
 def post_detail(request, year, month, day, post):
