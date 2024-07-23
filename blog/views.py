cbcce66869ccbc4a5edd78157b86a9867ac59417
Flynotfly <Alteria2004@gmail.com>
2024-07-23 10:23:09 +0400

    return to post_list function view from class

--- blog/views.py
+++ blog/views.py
@@ -1,37 +1,37 @@
-# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404, render
-from django.views.generic import ListView
+# from django.views.generic import ListView
 from django.views.decorators.http import require_POST
 from .models import Post
 from .forms import EmailPostForm, CommentForm
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 5
-    template_name = 'blog/post/list.html'
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 5
+#     template_name = 'blog/post/list.html'
 
 
-# def post_list(request):
-#     post_list = Post.published.all()
-#     # Pagination with 5 post per page
-#     paginator = Paginator(post_list, 5)
-#     page_number = request.GET.get('page', 1)
-#     try:
-#         posts = paginator.page(page_number)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         # get last page if page_number out of range
-#         posts = paginator.page(paginator.num_pages)
-#
-#     return render(
-#         request,
-#         'blog/post/list.html',
-#         {'posts': posts}
-#     )
+def post_list(request):
+    post_list = Post.published.all()
+    # Pagination with 5 post per page
+    paginator = Paginator(post_list, 5)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        # get last page if page_number out of range
+        posts = paginator.page(paginator.num_pages)
+
+    return render(
+        request,
+        'blog/post/list.html',
+        {'posts': posts}
+    )
 
 
 def post_detail(request, id, post):
