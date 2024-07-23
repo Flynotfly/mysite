a18c11083da19be2dac2972ab49cc323a92baa56
Flynotfly <Alteria2004@gmail.com>
2024-07-23 10:26:45 +0400

    add slash to the post_share path

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ app_name = 'blog'
 urlpatterns = [
     # post views
     path('', views.PostListView.as_view(), name='post_list'),
-    path('post/<int:id>/<slug:post>', views.post_detail, name='post_detail'),
+    path('post/<int:id>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
 ]
